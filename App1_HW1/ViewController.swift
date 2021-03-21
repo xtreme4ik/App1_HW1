21123ce5b4f1ccf0f46a798bfa6d2a64bf07380e
NovikYehor <yehorick@gmail.com>
2021-03-21 23:23:08 +0200

    Initial Commit

---
+++ App1_HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App1_HW1
+//
+//  Created by admin2 on 21.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
