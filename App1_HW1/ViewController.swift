005b293042b0dda89e1e8616de6d48e12a651ce2
NovikYehor <yehorick@gmail.com>
2021-03-22 14:30:31 +0200

    Done App

--- App1_HW1/ViewController.swift
+++ App1_HW1/ViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIButton!
+    @IBOutlet weak var yellowLight: UIButton!
+    @IBOutlet weak var greenLight: UIButton!
+    @IBOutlet weak var mainButton: UIButton!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        yellowLight.layer.cornerRadius = 50
+        redLight.layer.cornerRadius = 50
+        greenLight.layer.cornerRadius = 50
+
+    }
+    @IBAction func buttonPressed() {
+        if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        } else if yellowLight.alpha == 1 {
+              yellowLight.alpha = 0.3
+              greenLight.alpha = 1
+          } else if greenLight.alpha == 1 {
+                greenLight.alpha = 0.3
+                redLight.alpha = 1
+          } else {
+            mainButton.setTitle("NEXT", for: .normal)
+            redLight.alpha = 1
+          }
     }
 
 
